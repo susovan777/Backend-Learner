f6ff2955f479164cc2bbb868d9fc523cb8ce243a
Susovan <susovan.sahoo777@gmail.com>
2024-03-04 13:29:40 +0530

    Written a first code to create  a server

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,1 +1,20 @@
 // Creating a server using NodeJS
+
+const http = require("http");
+const port = 8081;
+
+const server = http.createServer((request, response) => {
+  // set response status code and response header
+  response.writeHead(200, { "Content-type": "text/html" });
+
+  // set response body i.e. data to be send
+  response.write("<h1>My Server</h1>");
+  response.write("<p>Created by Susovan in <b>Local machine</b></p>");
+
+  // tell the server that reposnse is complete and close the server
+  response.end();
+});
+
+server.listen(port, () => {
+    console.log(`NodeJS server started at the port ${port}`);
+})
