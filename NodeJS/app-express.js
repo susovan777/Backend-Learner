5b7e922ecbdb064fe86bc1527581882b1e91f8f3
Susovan <susovan.sahoo777@gmail.com>
2024-03-08 15:47:55 +0530

    Made a POST request

--- NodeJS/app-express.js
+++ NodeJS/app-express.js
@@ -1,14 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+app.use(express.json()); // to parse the request body as json
+
 const port = 8081;
 
-const todos = ["Complete nodeJS byte", "Play COC" ]
+const todos = ["Complete nodeJS byte", "Play COC"];
 
-app.get('/todos', (req, res) => {
-    // res.send('Hello world, from my local server');
-    res.send(todos);
+// get request
+// curl -v http://localhost:8081/todos
+app.get("/todos", (req, res) => {
+  // res.send('Hello world, from my local server');
+  res.send(todos);
+});
+
+// post request
+// curl -v -X POST -d '{"name": "Plan for next week"}' http://localhost:8081/todos -H 'Content-Type: application/json'
+app.post("/todos", (req, res) => {
+  let newTodo = req.body.name;
+  todos.push(newTodo);
+  res.status(201).end();
 });
 
 app.listen(port, () => {
-    console.log(`Express server started in the port ${port}`);
-})
+  console.log(`Express server started in the port ${port}`);
+});
