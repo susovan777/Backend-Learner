15712d83ea98ab0bbe994e7b2bcd6160d16c621e
Susovan <susovan.sahoo777@gmail.com>
2024-03-09 16:14:00 +0530

    Extending API's: MERN-2 Activity-5

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
 // console.log("nodemon working fine!");
 
 const http = require("http");
+const currencies = require('./currencies.json');
 const port = 8081;
 
 const server = http.createServer((req, res) => {
-  const serverInfo = {
+  /* const serverInfo = {
     serverName: "My Server",
     version: "1.0.0",
     currentDate: new Date().toLocaleDateString(),
@@ -22,6 +23,27 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-type": "text/html" });
     res.write("<h1>This is Homepage</h1>");
     res.end();
+  } */
+
+  switch (req.url) {
+    case "/": {
+      res.writeHead(200, { "Content-type": "text/html" });
+      res.write("<h1>Currency Database</h1>");
+      res.end();
+      break;
+    }
+
+    case "/currencies": {
+      res.writeHead(200, { "Content-type": "application/json" });
+      res.write(JSON.stringify(currencies));
+      res.end();
+      break;
+    }
+
+    default: {
+      res.writeHead(404);
+      res.end();
+    }
   }
 });
 
