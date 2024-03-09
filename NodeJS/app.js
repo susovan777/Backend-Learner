6afce69b151fede5894cc95bb3722d3b7de31596
Susovan <susovan.sahoo777@gmail.com>
2024-03-09 15:24:27 +0530

    Modified app.js send data to client

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -4,12 +4,21 @@ const http = require("http");
 const port = 8081;
 
 const server = http.createServer((request, response) => {
+  const serverInfo = {
+    serverName: "my server",
+    version: "1.0.0",
+    currentDate: new Date().toLocaleDateString(),
+    currentTime: new Date().toLocaleTimeString()
+  }
+
   // set response status code and response header
-  response.writeHead(200, { "Content-type": "text/html" });
+  // response.writeHead(200, { "Content-type": "text/html" });
+  response.writeHead(200, { "Content-type": "application/json" });
 
   // set response body i.e. data to be send
-  response.write("<h1>My Server</h1>");
-  response.write("<p>Created by Susovan in <b>Local machine</b></p>");
+  // response.write("<h1>My Server</h1>");
+  // response.write("<p>Created by Susovan in <b>Local machine</b></p>");
+  response.write(JSON.stringify(serverInfo));
 
   // tell the server that reposnse is complete and close the server
   response.end();
