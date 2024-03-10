fa17a22306baaca9cf2620a57d0e85d009386053
Susovan <susovan.sahoo777@gmail.com>
2024-03-10 20:33:02 +0530

    Learned controller and implemented ✅

--- Controller/currencies.controller.js
+++ Controller/currencies.controller.js
@@ -1,9 +1,26 @@
-const currencies = require('../currencies.json');
+const currencies = require("../currencies.json");
 
 const getCurrencyList = (req, res) => {
-    res.json(currencies);
-}
+  console.log("Current Path: /currencies");
+  res.json(currencies);
+};
 
+const getCurrencyWithSymbol = (req, res) => {
+  console.log("Current Path: /symbol");
+  const symbol = req.params.symbol;
+  console.log(symbol);
+
+  const result = currencies.data.find(
+    (ele) => ele.id.toLocaleLowerCase() === symbol.toLocaleLowerCase()
+  );
+  console.log(result);
+
+  if (result) {
+    res.json(result);
+  } else {
+    res.json(currencies);
+  }
+};
 
 // exporting the module
-module.exports = getCurrencyList;
+module.exports = { getCurrencyList, getCurrencyWithSymbol };
--- index-controller.js
+++ index-controller.js
@@ -1,15 +1,20 @@
 const express = require('express');
-const getCurrencyList = require('./Controller/currencies.controller');
+const currencies = require('./Controller/currencies.controller');
 
 const port = 8081;
 const app = express();
 
+// default page
 app.get('/', (req, res) => {
     res.write('<h1>Currency Database</h1>');
     res.end();
 })
 
-app.get('/currencies', getCurrencyList);
+// currencies page
+app.get('/currencies', currencies.getCurrencyList);
+
+// currency with symbol
+app.get('/currencies/:symbol', currencies.getCurrencyWithSymbol)
 
 app.listen(port, () => {
     console.log("Server started at the port", port);
