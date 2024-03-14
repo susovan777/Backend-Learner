79e57243944829adb13256715ceb374cdfbb83d1
Susovan <susovan.sahoo777@gmail.com>
2024-03-14 17:29:37 +0530

    Installed Joi validator and used validation in the users.controller.js file

--- Controller/users.controller.js
+++ Controller/users.controller.js
@@ -1,4 +1,15 @@
 const userDatas = require("../users.json");
+// adding Joi validator
+const joi = require("joi");
+// creating joi schema
+const schema = joi
+  .object()
+  .keys({
+    // age should be a number and integer
+    gender: joi.string().valid("male", "female"),
+    age: joi.number().integer().min(0).max(100),
+  }) // specifying atleast one of the key shoud be present
+  .or("gender", "age"); 
 
 const getAllUsers = (req, res) => {
   console.log("Current path: /users");
@@ -22,35 +33,42 @@ const searchUsersByQuery = (req, res) => {
   const age = req.query.age;
   console.log(`Current path: /search where age: ${age} and gender: ${gender}`);
 
-  if (!age && !gender) {
+  // adding validation using joi
+  const result = schema.validate({ gender, age });
+  if (result.error) {
+    return res.status(422).json(result.error);
+  }
+
+  // removed the manual validation
+  /* if (!age && !gender) {
     res.status(422).json({
       message: "Missing Search Parameters, search using age and/or gender",
     });
-  } else if (gender && age) {
+  } else  */ if (gender && age) {
     const result = userDatas.data.filter(
       (user) => user.gender === gender && user.dob.age >= age
     );
     res.json(result);
   } else if (gender) {
-    if (!["male", "female"].includes(gender.toLowerCase())) {
+    /* if (!["male", "female"].includes(gender.toLowerCase())) {
       res
         .status(422)
         .json({ message: "Gender to search can either be 'male' or 'female'" });
     } else {
-      const result = userDatas.data.filter((user) => user.gender === gender);
-      res.json(result);
-    }
+    } */
+    const result = userDatas.data.filter((user) => user.gender === gender);
+    res.json(result);
   } else if (age) {
-    if (!Number(age)) {
+    /* if (!Number(age)) {
       res.status(422).json({ message: "Age parameter should be a number" });
     } else if (age < 0 || age > 100) {
       res.status(422).json({
         message: "Age out of bounds. It should be a number between 0 and 100",
       });
     } else {
-      const result = userDatas.data.filter((user) => user.dob.age >= age);
-      res.json(result);
-    }
+    } */
+    const result = userDatas.data.filter((user) => user.dob.age >= age);
+    res.json(result);
   } else res.sendStatus(404);
 };
 
