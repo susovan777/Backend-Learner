a3323d9bd103333407dc9a4be8cd7590394dc763
Susovan <susovan.sahoo777@gmail.com>
2024-03-22 13:52:47 +0530

    Learned CRUD operation on database

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -21,7 +21,48 @@ db.collection_name.find({filter}).pretty()
 // Create Document
 db.collection_name.insertOne({})
 db.collection_name.insertMany([{}, {},...])
+example: 
+db.inventory.insertMany([
+    { item: "journal", qty: 25, size: { h: 14, w: 21, uom: "cm" }, status: "A" },
+    { item: "notebook", qty: 50, size: { h: 8.5, w: 11, uom: "in" }, status: "A" },
+    { item: "paper", qty: 100, size: { h: 8.5, w: 11, uom: "in" }, status: "D" },
+    { item: "planner", qty: 75, size: { h: 22.85, w: 30, uom: "cm" }, status: "D" },
+    { item: "postcard", qty: 45, size: { h: 10, w: 15.25, uom: "cm" }, status: "A" }
+ ]);
 
+//  Read document
+Example:
+db.inventory.find({status: "D"})
 
+// Update Document
+db.collection_name.updateOne({filter}, {$set: {}})
+db.collection_name.updateMany()
+Example:
+db.inventory.updateOne(
+    { item: "paper" }, // filter
+    // update
+    {
+      $set: { "size.uom": "cm", status: "P" },
+      $currentDate: { lastModified: true }
+    }
+ )
 
+ Example:
+ db.inventory.updateMany(
+    { "qty": { $lt: 50 } }, // filter
+    // update
+    {
+      $set: { "size.uom": "in", status: "P" },
+      $currentDate: { lastModified: true }
+    }
+ )
+Example: // For replace
+db.inventory.replaceOne(
+    { item: "paper" },
+    { item: "paper", instock: [ { warehouse: "A", qty: 60 }, { warehouse: "B", qty: 40 } ] }
+ )
 
+// Delete document
+Example:
+db.inventory.deleteOne({ status : "A" }) // delete first document
+db.inventory.deleteMany({ status : "A" })
