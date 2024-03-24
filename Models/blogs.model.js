829d367e070a5555ef73612a34d8b85443e8d7d2
Susovan <susovan.sahoo777@gmail.com>
2024-03-25 01:39:15 +0530

    Installed validator package and added validation to authors email and image

--- Models/blogs.model.js
+++ Models/blogs.model.js
@@ -1,6 +1,33 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-// defining schema
+// defining schema for author: 👉 Intro to Nested Schema
+const authorSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, maxLength: 25 },
+    twitterHandle: { type: String },
+    email: {
+      type: String,
+      required: true,
+      maxLength: 50,
+      validate: (value) => validator.isEmail(value), // returns boolean value
+    },
+    // adding valdattion to image
+    image: {
+      type: String,
+      validate: (value) =>
+        validator.isURL(value, {
+          require_host: true,
+          require_protocol: true,
+        }),
+    },
+  },
+  {
+    _id: false, // while posting to db unneccessary id value was added
+  }
+);
+
+// defining schema for blog
 const blogSchema = new mongoose.Schema(
   {
     /* title: String,
@@ -9,7 +36,7 @@ const blogSchema = new mongoose.Schema(
   publishedAt: Date,
    */
     title: { type: String, unique: true, required: true },
-    authors: { type: [String] },
+    authors: [authorSchema],
     content: { type: String, default: "" },
     publishedAt: { type: Date, default: null },
   },
