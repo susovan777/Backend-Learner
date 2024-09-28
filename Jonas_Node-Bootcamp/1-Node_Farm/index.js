57b435cd2a815522d2810bfb31d6d3730eb68870
Susovan <susovan.sahoo777@gmail.com>
2024-09-28 22:25:53 +0530

    Create a new file using node writeFileSync() method

--- Jonas_Node-Bootcamp/1-Node_Farm/index.js
+++ Jonas_Node-Bootcamp/1-Node_Farm/index.js
@@ -1,6 +1,13 @@
 // const fs = require("fs");
-import { readFileSync } from "node:fs";
-
 // fs.readFileSync(path[, options])
+import { readFileSync, writeFileSync } from "node:fs";
+
+// reading from a file
 const text = readFileSync("./txt/input.txt", "utf-8");
 console.log(text);
+
+const textOutput = `This is what we know about Avocado: ${text}. \nCreated on ${Date.now()}`;
+
+// writting to a file
+writeFileSync("./txt/output.txt", textOutput);
+console.log("File Written!");
