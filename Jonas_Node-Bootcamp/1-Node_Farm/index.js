82e35f8525647adaf89ab899ddcf9f829dadbf40
Susovan <susovan.sahoo777@gmail.com>
2024-09-29 11:10:26 +0530

    Created a server and used routing

--- Jonas_Node-Bootcamp/1-Node_Farm/index.js
+++ Jonas_Node-Bootcamp/1-Node_Farm/index.js
@@ -1,7 +1,9 @@
-// const fs = require("fs");
+//////////////////     FILE      ////////////////////////
+
+// const fs = require("fs"); // for CJS
 // fs.readFileSync(path[, options])
 // ⭐ Blocking, Synchronous way ⭐
-import { readFile, readFileSync, writeFile, writeFileSync } from "node:fs";
+import { readFile, readFileSync, writeFile, writeFileSync } from "node:fs"; // for ESM
 // reading from a file
 const text = readFileSync("./txt/input.txt", "utf-8");
 // console.log(text);
@@ -12,15 +14,31 @@ const textOutput = `This is what we know about Avocado: ${text}. \nCreated on ${
 // console.log("File Written!");
 
 // ⭐ Non-blocking, asynchronous way ⭐
-readFile("./txt/start.txt", "utf-8", (error, data) => {
-  readFile(`./txt/${data}.txt`, "utf-8", (error, data2) => {
-    console.log(data2);
-    readFile("./txt/append.txt", "utf-8", (error, data3) => {
-      console.log(data3);
-      writeFile("./txt/output.txt", `${data2}\n${data3}`, "utf-8", (error) => {
-        console.log("File has been written!");
-      });
-    });
-  });
+// readFile("./txt/start.txt", "utf-8", (error, data) => {
+//   readFile(`./txt/${data}.txt`, "utf-8", (error, data2) => {
+//     console.log(data2);
+//     readFile("./txt/append.txt", "utf-8", (error, data3) => {
+//       console.log(data3);
+//       writeFile("./txt/output.txt", `${data2}\n${data3}`, "utf-8", (error) => {
+//         console.log("File has been written!");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read the file!");
+
+// //////////////////////     SERVER     /////////////////////////////
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  //   console.log(req.url);
+  const path = req.url;
+
+  if (path === "/" || path === "/home") res.end("This is HOME page");
+  else if (path === "/product") res.end("This is PRODUCT page");
+  else res.end("Page not found!");
 });
-console.log("Will read the file!");
+
+server.listen(3000, () => {
+  console.log("Listening to server at port 3000...");
+});
