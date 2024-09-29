32fe45df0ae969011c5784df9c008436c3a694c8
Susovan <susovan.sahoo777@gmail.com>
2024-09-29 10:20:59 +0530

    Read and write file asynchronously

--- Jonas_Node-Bootcamp/1-Node_Farm/index.js
+++ Jonas_Node-Bootcamp/1-Node_Farm/index.js
@@ -1,13 +1,26 @@
 // const fs = require("fs");
 // fs.readFileSync(path[, options])
-import { readFileSync, writeFileSync } from "node:fs";
-
+// ⭐ Blocking, Synchronous way ⭐
+import { readFile, readFileSync, writeFile, writeFileSync } from "node:fs";
 // reading from a file
 const text = readFileSync("./txt/input.txt", "utf-8");
-console.log(text);
+// console.log(text);
 
 const textOutput = `This is what we know about Avocado: ${text}. \nCreated on ${Date.now()}`;
-
 // writting to a file
-writeFileSync("./txt/output.txt", textOutput);
-console.log("File Written!");
+// writeFileSync("./txt/output.txt", textOutput);
+// console.log("File Written!");
+
+// ⭐ Non-blocking, asynchronous way ⭐
+readFile("./txt/start.txt", "utf-8", (error, data) => {
+  readFile(`./txt/${data}.txt`, "utf-8", (error, data2) => {
+    console.log(data2);
+    readFile("./txt/append.txt", "utf-8", (error, data3) => {
+      console.log(data3);
+      writeFile("./txt/output.txt", `${data2}\n${data3}`, "utf-8", (error) => {
+        console.log("File has been written!");
+      });
+    });
+  });
+});
+console.log("Will read the file!");
