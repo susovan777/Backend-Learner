54acd9088654d4a4b8550ca6f457cdee87fa483f
Susovan <susovan.sahoo777@gmail.com>
2024-10-03 22:00:47 +0530

    Read local file and served to the client via express

--- Jonas_Node-Bootcamp/2-Natours_API/index.js
+++ Jonas_Node-Bootcamp/2-Natours_API/index.js
@@ -1,9 +1,15 @@
 import express from "express";
+import { readFileSync } from "node:fs";
 const app = express();
 
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
   //   res.send("Hello world!");
   res.status(200).json({ message: "Hello from the server", app: "Natours" });
+}); */
+
+const tours = JSON.parse(readFileSync("./data/tours.json"));
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({ status: "success", result: tours.length, data: { tours } });
 });
 
 app.listen(3000, () => {
