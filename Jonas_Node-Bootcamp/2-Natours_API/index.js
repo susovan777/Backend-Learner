83771f896b0dc479b05940a643a41fc3db7e7f3c
Susovan <susovan.sahoo777@gmail.com>
2024-10-05 08:32:19 +0530

    Learnt PATCH and DELETE request

--- Jonas_Node-Bootcamp/2-Natours_API/index.js
+++ Jonas_Node-Bootcamp/2-Natours_API/index.js
@@ -46,6 +46,30 @@ app.post("/api/v1/tours", (req, res) => {
   });
 });
 
+// PATCH request
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length)
+    res.status(404).json({ status: "Not found!", message: "Invalid ID" });
+  res.status(200).json({
+    status: "Success",
+    message: "Tour updated",
+    data: {
+      tour: "Tour update here",
+    },
+  });
+});
+
+// DELETE request
+app.delete("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length)
+    res.status(404).json({ status: "Not found!", message: "Invalid ID" });
+  res.status(204).json({
+    status: "Success",
+    message: "No content",
+    data: null,
+  });
+});
+
 app.listen(3000, () => {
   console.log("Listening to the server at port 3000...");
 });
