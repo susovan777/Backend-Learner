f93dab880a2e17023d33d923bb3620a91048d2d5
Susovan <susovan.sahoo777@gmail.com>
2025-09-21 20:57:41 +0530

    Promises

--- Jonas_Node-Bootcamp/09-Asynchronous_JS/index.js
+++ Jonas_Node-Bootcamp/09-Asynchronous_JS/index.js
@@ -5,7 +5,8 @@ import superagent from "superagent";
 fs.readFile(`dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
-  superagent
+  //   callback hell
+  /* superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
       if (err) return console.log(err.message);
@@ -15,5 +16,21 @@ fs.readFile(`dog.txt`, (err, data) => {
         if (err) return console.log(err.message);
         console.log("Random dog image saved!");
       });
+    }); */
+
+    // Using Promises
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((res) => {
+      if (err) return console.log(err.message);
+      console.log(res.body);
+
+      fs.writeFile("dog-img.txt", res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log("Random dog image saved!");
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
