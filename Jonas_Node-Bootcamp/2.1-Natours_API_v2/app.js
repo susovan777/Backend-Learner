9fc87a20f1b40ee4a9ef240db032ff9a40e46c1e
Susovan <susovan.sahoo777@gmail.com>
2025-10-04 21:26:40 +0530

    Handled POST request

--- Jonas_Node-Bootcamp/2.1-Natours_API_v2/app.js
+++ Jonas_Node-Bootcamp/2.1-Natours_API_v2/app.js
@@ -1,17 +1,48 @@
 import express from "express";
+import fs, { writeFile } from "fs";
+
 
 const app = express();
 const port = 3000;
+
+app.use(express.json()); // This middleware parses JSON data from the request body
+
+const tours = JSON.parse(
+  fs.readFileSync(`./data/tours-simple.json`) // in case of CJS we could use __dirname instead of .
+);
 
 app.get("/", (req, res) => {
   // res.status(200).send("Hello from the server!")
   res.status(200).json({ message: "Hello from the server!", app: "natours" });
 });
 
-app.post("/", (req, res) => {
-    console.log("We can post to this endpoint!"); // this will print to the console
-    res.send("We can post to this endpoint!") // this will print in the server
-})
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  // console.log("We can post to this endpoint!"); // this will print to the console
+  // res.send("We can post to this endpoint!"); // this will print in the server
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(`./data/tours-simple.json`, JSON.stringify(newTour), (err) =>
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    })
+  );
+});
 
 app.listen(port, () => {
   console.log("Listening to the server at", port);
