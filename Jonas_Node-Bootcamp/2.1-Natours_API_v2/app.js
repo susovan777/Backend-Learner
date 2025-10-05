41ba90deac43a5abb84e74375979238720e5851b
Susovan <susovan.sahoo777@gmail.com>
2025-10-05 11:21:14 +0530

    GET/POST/PATCH/DELETE route

--- Jonas_Node-Bootcamp/2.1-Natours_API_v2/app.js
+++ Jonas_Node-Bootcamp/2.1-Natours_API_v2/app.js
@@ -1,6 +1,5 @@
 import express from "express";
-import fs, { writeFile } from "fs";
-
+import fs from "fs";
 
 const app = express();
 const port = 3000;
@@ -11,11 +10,13 @@ const tours = JSON.parse(
   fs.readFileSync(`./data/tours-simple.json`) // in case of CJS we could use __dirname instead of .
 );
 
+// GET request
 app.get("/", (req, res) => {
   // res.status(200).send("Hello from the server!")
   res.status(200).json({ message: "Hello from the server!", app: "natours" });
 });
 
+// GET all tours
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
@@ -26,6 +27,30 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+// GET tour by Id
+app.get("/api/v1/tours/:id", (req, res) => {
+  console.log(req.params.id * 1);
+
+  const tourId = req.params.id * 1;
+  const tourById = tours.filter(el => el.id === tourId)
+
+  if (req.params.id > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    result: tours.length,
+    data: {
+      tour: tourById,
+    },
+  });
+});
+
+// POST request
 app.post("/api/v1/tours", (req, res) => {
   // console.log("We can post to this endpoint!"); // this will print to the console
   // res.send("We can post to this endpoint!"); // this will print in the server
@@ -33,6 +58,13 @@ app.post("/api/v1/tours", (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
+
+  if (!tourId) {
+    return res.status(401).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
 
   fs.writeFile(`./data/tours-simple.json`, JSON.stringify(newTour), (err) =>
     res.status(201).json({
@@ -42,6 +74,46 @@ app.post("/api/v1/tours", (req, res) => {
       },
     })
   );
+});
+
+// PATCH request
+app.patch("/api/v1/tours/:id", (req, res) => {
+  const tourId = req.params.id * 1;
+
+  // logic to update the data ....
+
+  if (tourId > tours.length) {
+    return res.status(401).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      // updated tour here
+    },
+  });
+});
+
+// DELETE request 
+app.delete("/api/v1/tours/:id", (req, res) => {
+  const tourId = req.params.id * 1;
+
+  // logic to delete the data ....
+
+  if (tourId > tours.length) {
+    return res.status(401).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
+
+  res.status(201).json({
+    status: "success",
+    data: null, // as of now no data to delete
+  });
 });
 
 app.listen(port, () => {
